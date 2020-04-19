86fcdde3182ab9b4e528306e76bcc5bf7987ac4f
Ruslan <voodooley@outlook.com>
2020-04-19 21:52:08 +0300

    после 3-го урока

--- django3/settings.py
+++ django3/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
